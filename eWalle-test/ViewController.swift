b5ecb96ac2a3dafb490a986c05ac22b7b8d64f46
marcoswmv <marcoswmvicente@gmail.com>
2020-07-11 15:43:35 +0300

    Initial Commit

---
+++ eWalle-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  eWalle-test
+//
+//  Created by Marcos Vicente on 11.07.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
